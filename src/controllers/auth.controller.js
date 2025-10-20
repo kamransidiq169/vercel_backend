ca67599350546ea0821ef73f6fa0c2bf62274058
kamran sidiq <kamransidiq169@gmail.com>
2025-10-20 08:55:25 +0530

    initial commit

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,32 +5,44 @@ import jwt from 'jsonwebtoken'
 
 // for simple user who can watch order and scroll
 
-export const registerUser=async(req,res)=>{
-    const {fullName, email, password}=req.body
-    
-   const isUserAlreadyExists= await userModel.findOne({email})
+export const registerUser = async (req, res) => {
+  try {
+    const { fullName, email, password } = req.body;
 
-   if(isUserAlreadyExists){
-    return res.status(400).send("user already exists")
-   }
-
-   const hashPassword = await bcrypt.hash(password,10)
-   
-   const user=userModel.create({fullName,email,password:hashPassword})
-
-  const token=jwt.sign({id:user._id},process.env.JWT_SECRET)
-
-   res.cookie("token",token)
-
-   res.status(201).json({
-    message:"user registered successfully",
-    user:{
-        _id:user._id,
-        email:user.email,
-        fullName:user.fullName
+    const isUserAlreadyExists = await userModel.findOne({ email });
+    if (isUserAlreadyExists) {
+      return res.status(400).send("User already exists");
     }
-   })
-}
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const user = await userModel.create({
+      fullName,
+      email,
+      password: hashPassword
+    });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None"
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        _id: user._id,
+        email: user.email,
+        fullName: user.fullName
+      }
+    });
+  } catch (err) {
+    console.error("Registration error:", err);
+    res.status(500).json({ message: "Server error during registration" });
+  }
+};
 
 export const loginUser = async (req,res)=>{
     const { email, password}=req.body
