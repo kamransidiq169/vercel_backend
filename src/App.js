314c7c0bc1a7ad039bf0f65f80e518c38f2553c1
kamran sidiq <kamransidiq169@gmail.com>
2025-10-23 18:45:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import cors from 'cors'
 const app = express()
 
 app.use(cookieParser())
-app.use(express.json())
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
+
 app.get("/", (req, res) => {
     res.send("hii I am kamran")
 })
